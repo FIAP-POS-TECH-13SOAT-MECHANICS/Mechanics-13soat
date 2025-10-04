8c589f536312e293e9a9aaef7fb4088ab6711740
Augusto César Bisognin <augustocb@gmail.com>
2025-10-04 12:40:29 -0300

    validação para consulta paginada e UpdateUserRequest

--- src/Mechanics.Application/Utils/PagedList/PaginatedListRequest.cs
+++ src/Mechanics.Application/Utils/PagedList/PaginatedListRequest.cs
@@ -8,13 +8,13 @@ public class PaginatedListRequest
     ///     Número da página, iniciando em 1.
     /// </summary>
     /// <example>1</example>
-    [Range(1, int.MaxValue)]
+    [Range(1, int.MaxValue, ErrorMessage = "Page must be greater than 0.")]
     public required int Page { get; init; } = 1;
 
     /// <summary>
     ///     Quantidade de itens por página.
     /// </summary>
     /// <example>10</example>
-    [Range(1, int.MaxValue)]
+    [Range(1, int.MaxValue, ErrorMessage = "Items per page must be greater than 0.")]
     public required int ItemsPerPage { get; init; } = 10;
 }

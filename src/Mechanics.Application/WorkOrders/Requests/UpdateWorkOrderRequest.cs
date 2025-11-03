5067f5c8e84af461c9a2bd3e0d3a5af6eb935567
lucas esteves <esteveslucasreis@gmail.com>
2025-11-03 12:54:07 -0300

    feature(workorder): adicionando update (services, products, observations) endpoint

---
+++ src/Mechanics.Application/WorkOrders/Requests/UpdateWorkOrderRequest.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Mechanics.Application.WorkOrders.Requests;
+
+public class UpdateWorkOrderRequest
+{
+    public IEnumerable<Guid>? ProductIds { get; init; }
+    public IEnumerable<Guid>? ServiceIds { get; init; }
+    public string? Observations { get; init; }
+}

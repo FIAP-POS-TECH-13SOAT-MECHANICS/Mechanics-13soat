c8bc82b3a0fce9e5b5a5de9c092b6e740bf6baeb
Augusto César Bisognin <augustocb@gmail.com>
2025-12-19 18:06:47 -0300

    ajuste nas permissões

--- infra/k8s.tf
+++ infra/k8s.tf
@@ -1,13 +1,25 @@
-data "aws_iam_role" "lab_role" {
-  name = "LabRole"
+data "aws_iam_roles" "lab_eks_cluster_roles" {
+  name_regex = ".*LabEksClusterRole.*"
+}
+
+data "aws_iam_role" "lab_cluster_role" {
+  name = one(data.aws_iam_roles.lab_eks_cluster_roles.names)
+}
+
+data "aws_iam_roles" "lab_eks_node_roles" {
+  name_regex = ".*LabEksNodeRole.*"
+}
+
+data "aws_iam_role" "lab_node_role" {
+  name = one(data.aws_iam_roles.lab_eks_node_roles.names)
 }
 
 resource "aws_eks_cluster" "cluster" {
   name     = "${local.prefix}-cluster"
-  role_arn = data.aws_iam_role.lab_role.arn
+  role_arn = data.aws_iam_role.lab_cluster_role.arn
 
   access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
     bootstrap_cluster_creator_admin_permissions = true
   }
 
@@ -15,8 +27,8 @@ resource "aws_eks_cluster" "cluster" {
 
   compute_config {
     enabled       = true
-    node_pools    = ["general-purpose"]
-    node_role_arn = data.aws_iam_role.lab_role.arn
+    node_pools    = ["general-purpose", "system"]
+    node_role_arn = data.aws_iam_role.lab_node_role.arn
   }
 
   kubernetes_network_config {
@@ -32,6 +44,16 @@ resource "aws_eks_cluster" "cluster" {
   }
 
   vpc_config {
-    subnet_ids = aws_subnet.private[*].id
+    subnet_ids = concat(
+      aws_subnet.private.*.id,
+      aws_subnet.public.*.id,
+    )
+
+    endpoint_private_access = true
+    endpoint_public_access  = local.public
+  }
+
+  upgrade_policy {
+    support_type = "STANDARD"
   }
 }

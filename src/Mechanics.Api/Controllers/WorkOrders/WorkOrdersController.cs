b52ad77e265c6a409557941d0465f785df66eec1
Augusto César Bisognin <augustocb@gmail.com>
2026-02-28 02:48:58 -0300

    correção nos seedes de DEV e permissões

--- src/Mechanics.Api/Controllers/WorkOrders/WorkOrdersController.cs
+++ src/Mechanics.Api/Controllers/WorkOrders/WorkOrdersController.cs
@@ -17,8 +17,7 @@ namespace Mechanics.Api.Controllers.WorkOrders;
 [ApiExplorerSettings(GroupName = "v1")]
 [Route("api/[controller]")]
 [Authorize(Policy = PolicyNames.EmployeesOnly)]
-public class WorkOrdersController(WorkOrderAppService workOrderService)
-    : ControllerBase
+public class WorkOrdersController(WorkOrderAppService workOrderService) : ControllerBase
 {
     /// <summary>
     ///     Cria uma nova ordem de serviço.
@@ -138,26 +137,6 @@ public class WorkOrdersController(WorkOrderAppService workOrderService)
 
         await workOrderService.UpdateDetails(id, request, userId, cancellationToken);
         return NoContent();
-    }
-
-    /// <summary>
-    ///     Consulta da ordem de serviço pela chave de acesso.
-    /// </summary>
-    /// <param name="accessKey">Código de acesso de 8 dígitos fornecido ao cliente.</param>
-    /// <param name="cancellationToken">Token para cancelamento da operação.</param>
-    /// <response code="200">Resultado encontrado.</response>
-    /// <response code="404">Não encontrado.</response>
-    [HttpGet("track")]
-    [Authorize(Policy = PolicyNames.CustomersOnly)]
-    [Produces("application/json", Type = typeof(GetWorkOrderResponse))]
-    [ProducesResponseType(typeof(GetWorkOrderResponse), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> Track(string accessKey, CancellationToken cancellationToken)
-    {
-        var customerId = Guid.Parse(User.Claims.First(c => c.Type == "customerId").Value);
-
-        var response = await workOrderService.TrackByAccessKey(customerId, accessKey, cancellationToken);
-        return response is not null ? Ok(response) : NotFound();
     }
 
     /// <summary>
